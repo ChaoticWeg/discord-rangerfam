8603c05115a75d9bbfae8bddc83880e4fc002df5
ChaoticWeg <shawn@chaoticweg.cc>
2019-01-11 11:34:44 -0600

    add role removal and blanket role adding

--- lib/bot/bot.py
+++ lib/bot/bot.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from lib import config, logging
+from lib import config, logging, errors
 
 import discord
 
@@ -46,3 +46,34 @@ class WegBot(commands.Bot):
     def remove_eligible_role(self, name):
         prev_roles = [role for role in self._eligible_roles if not role.lower() == name.lower()]
         self.eligible_roles = prev_roles
+
+    def find_role_by_name(self, ctx, name):
+        results = [role for role in ctx.guild.roles if role.name.lower() == name.lower()]
+        return None if len(results) < 1 else results[0]
+
+    async def give_role(self, ctx, name):
+        self.logger.debug(f"Trying to give role '{name}' to {ctx.author} in {ctx.guild}")
+        role = self.find_role_by_name(ctx, name)
+        if role is None:
+            raise errors.RoleNotFoundError(name)
+        if not role.name.lower() in self.eligible_roles:
+            raise errors.IneligibleRoleError(role)
+        add_reason = f"Role '{role.name}' requested by {ctx.author}"
+        await ctx.author.add_roles(role, reason=add_reason)
+
+    async def give_all_roles(self, ctx):
+        roles = [self.find_role_by_name(ctx, name) for name in self.eligible_roles]
+        for role in roles:
+            if role is None:
+                raise errors.RoleNotFoundError("unknown")
+        add_reason = f"All roles requested by {ctx.author}"
+        await ctx.author.add_roles(*roles, reason=add_reason)
+
+    async def remove_role(self, ctx, name):
+        role = self.find_role_by_name(ctx, name)
+        if role is None:
+            raise errors.RoleNotFoundError(name)
+        if not role.name.lower() in self.eligible_roles:
+            raise errors.IneligibleRoleError(role)
+        rem_reason = f"Role '{role.name}' requested to be removed by {ctx.author}"
+        await ctx.author.remove_roles(role, reason=rem_reason)
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from lib import bot as wegbot
+from lib import bot as wegbot, errors as wegbot_errors
 import discord
 
 bot = wegbot.WegBot()
 
 OK_EMOJI_CODE = u"\u2705"
 NO_EMOJI_CODE = u"\u274C"
-
 _newline = '\n'
-role_description = f"""Request a role by name.
 
-Roles that can be requested are:
+addrole_description = f"""Roles that can be requested are:
 - {(_newline + '- ').join(bot.eligible_roles)}
 """
 
-@bot.command(usage='<role>', description=role_description)
-async def role(ctx, *, role: str):
+@bot.command(usage='<role>', description=addrole_description)
+async def addrole(ctx, *, role: str):
     """ Request a role by name. """
 
     # must be in eligible text channel
@@ -25,38 +23,34 @@ async def role(ctx, *, role: str):
 
     logger.info(f"{ctx.author} in guild '{ctx.guild}' wants a role called {role}")
     async with ctx.typing():
-        role_to_add = None
-
-        for this_role in ctx.guild.roles:
-            if this_role.name.lower() == role.lower():
-                role_to_add = this_role
+        if role.lower() == 'all':
+            await bot.give_all_roles(ctx)
+            await ctx.send(f"Gave all roles to {ctx.author.mention}. Probably.")
+        else:
+            await bot.give_role(ctx, role)
+            role_to_add = bot.find_role_by_name(ctx, role)
+            await ctx.send(f"Gave the role `{role_to_add.name}` to {ctx.author.mention}")
         
-        if role_to_add is None:
-            logger.info(f"Role '{role}' does not exist in guild '{ctx.guild}'.")
-            await ctx.message.add_reaction(NO_EMOJI_CODE)
-            await ctx.send(f"I don't see a role with the name '{role}', {ctx.author.mention}.")
-            return
-        
-        eligible_roles = [role.name.lower() for role in ctx.guild.roles if role.name.lower() in bot.eligible_roles]
-        if not role_to_add.name.lower() in eligible_roles:
-            logger.info(f"{ctx.author} requested ineligible role '{role_to_add.name}'")
-            logger.debug(f"eligible roles: [{', '.join(eligible_roles)}]")
-            await ctx.message.add_reaction(NO_EMOJI_CODE)
-            await ctx.send(f"The role `{role_to_add.name}` is not eligible to be requested, {ctx.author.mention}.")
-            return
-        
-        add_reason = f"Role '{role}' requested by {ctx.author}"
-        await ctx.author.add_roles(role_to_add, reason=add_reason)
-
         await ctx.message.add_reaction(OK_EMOJI_CODE)
-        await ctx.send(f"Gave the role `{role_to_add.name}` to {ctx.author.mention}")
 
         logger.info(f"Successfully gave role {role_to_add.name} to {ctx.author}")
 
-@role.error
-async def role_error(ctx, err):
-    """ ?role broke something... """
+@addrole.error
+async def addrole_error(ctx, err):
+    """ ?addrole broke something... """
     await ctx.message.add_reaction(NO_EMOJI_CODE)
+
+    # role does not exist
+    if isinstance(err, wegbot_errors.RoleNotFoundError):
+        bot.logger.info(f"Role '{err.role_name}' does not exist")
+        await ctx.send(f"There's no role here called '{err.role_name}', {ctx.author.mention}.")
+        return
+    
+    # role is ineligible
+    if isinstance(err, wegbot_errors.IneligibleRoleError):
+        bot.logger.warn(f"{ctx.author.mention} requested ineligible role '{err.role.name}' in {ctx.guild}")
+        await ctx.send(f"`{err.role.name}` can't be requested, {ctx.author.mention}.")
+        return
 
     # permission errors
     permission_errors = [discord.errors.Forbidden, discord.ext.commands.BotMissingPermissions, discord.ext.commands.MissingPermissions]
@@ -69,6 +63,32 @@ async def role_error(ctx, err):
     # unknown error
     await ctx.send("Error! I just broke; that's all I know. Please send Weg.")
     bot.logger.error(err)
+
+removerole_description = f"""Roles that can be requested are:
+- {(_newline + '- ').join(bot.eligible_roles)}
+"""
+
+@bot.command(usage='<role>', description=removerole_description)
+async def removerole(ctx, *, role: str):
+    """ Remove a role by name. """
+
+    # must be in eligible text channel
+    if not ctx.message.channel.id in bot.eligible_channels:
+        return
+    
+    logger = bot.logger
+
+    logger.info(f"{ctx.author} in guild '{ctx.guild}' wants to remove a role called {role}")
+    async with ctx.typing():
+        if role.lower() == 'all':
+            await ctx.message.add_reaction(NO_EMOJI_CODE)
+            await ctx.send(f"Can't request to have all roles removed, {ctx.author.mention}.")
+            return
+        await bot.remove_role(ctx, role.lower())
+        await ctx.message.add_reaction(OK_EMOJI_CODE)
+        role_to_add = bot.find_role_by_name(ctx, role)
+        await ctx.send(f"Removed the role `{role_to_add.name}` from {ctx.author.mention}")
+
 
 if __name__ == "__main__":
     bot.run()
