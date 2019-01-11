dfea8ec55eb365cfee75e5ec6d1659d22356c388
ChaoticWeg <shawn@chaoticweg.cc>
2019-01-10 23:40:35 -0600

    add failure message and react if role does not exist

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ async def role(ctx, *, role: str):
                 role_to_add = this_role
         
         if role_to_add is None:
+            print(f"Role '{role}' does not exist in this guild.")
+            await ctx.message.add_reaction(NO_EMOJI_CODE)
             await ctx.send(f"I don't see a role with the name '{role}', {ctx.author.mention}.")
             return
         
-        roles = [role for role in ctx.guild.roles if role.name.lower() in bot.eligible_roles]
-        if not role_to_add.name.lower() in roles:
+        eligible_roles = [role for role in ctx.guild.roles if role.name.lower() in bot.eligible_roles]
+        if not role_to_add.name.lower() in eligible_roles:
             print(f"{ctx.author} requested ineligible role '{role_to_add.name}'")
             await ctx.message.add_reaction(NO_EMOJI_CODE)
             await ctx.send(f"The role `{role_to_add.name}` is not eligible to be requested, {ctx.author.mention}.")
