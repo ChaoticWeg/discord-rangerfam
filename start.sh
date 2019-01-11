6607a3971892dd0ac0c6804925d9d0b3091b139a
ChaoticWeg <shawn@chaoticweg.cc>
2019-01-11 00:42:37 -0600

    pkill for great justice

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# kill existing processes with extreme prejudice
+pkill -f discord-rangerfam
+
+# some setup
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 runalways="${thisdir}/run_always.sh"
 logfile="${thisdir}/run.log"
 
+# run run_always in the background, ignoring sighup
 nohup bash "${runalways}" >"${logfile}" 2>&1 &
-
